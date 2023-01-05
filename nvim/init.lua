7b583c470ad3632df84e97a2a036b05ce36db060
Adam Bazzi <adambazzi@proton.me>
2023-01-05 12:03:07 -0500

    added tab rules to init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,6 +4,14 @@ vim.api.nvim_set_keymap('i','jk','<ESC>', {noremap = true})
 
 vim.o.relativenumber = true
 
+local set = vim.opt -- set options
+set.tabstop = 4
+set.softtabstop = 4
+set.shiftwidth = 4
+
+
+require('packer-plugins')
+
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
@@ -11,6 +19,5 @@ vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
 
-require('packer-plugins')
 require('mason').setup()
 require('treesitter')
