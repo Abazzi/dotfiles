9439162f2a404dbda47a7f902a42c629850e47e4
Adam Bazzi <8884041+Abazzi@users.noreply.github.com>
2023-11-07 13:31:10 -0500

    Added .gitignore file for node projects added basic prettier config Created script that sets up a directory with eslint, prettier, git, and basic starter files

---
+++ webDirGen.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+mkdir $1
+
+cd $1
+
+## Copy Favicon, gitignore and prettier config 
+cp ~/dotfiles/favicon.ico favicon.ico
+cp ~/dotfiles/.gitignore .gitignore
+cp ~/dotfiles/.prettierrc .prettierrc
+
+## Create starter files
+touch index.html
+touch main.js
+touch styles.css
+
+## initalize git repo and install eslint and prettier
+git init
+npm init -y
+npm install -d eslint prettier eslint-config-prettier
