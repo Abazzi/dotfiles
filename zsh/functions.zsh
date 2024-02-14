41a886683d96b70bc1c8ed9a49c85ffa3d58e906
Adam Bazzi <8884041+Abazzi@users.noreply.github.com>
2024-02-14 16:06:45 -0500

    Updated tmux config to be more compatible with nvim plugin

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -6,15 +6,17 @@
 # export LANG=en_US.UTF-8
 #VI Mode
 # vi mode
+zstyle ':completion:*' menu select
+zmodload zsh/complist
 bindkey -v
 export KEYTIMEOUT=1
 
-# # Use vim keys in tab complete menu:
-# bindkey -M menuselect 'h' vi-backward-char
-# bindkey -M menuselect 'k' vi-up-line-or-history
-# bindkey -M menuselect 'l' vi-forward-char
-# bindkey -M menuselect 'j' vi-down-line-or-history
-# bindkey -v '^?' backward-delete-char
+# Use vim keys in tab complete menu:
+bindkey -M menuselect 'h' vi-backward-char
+bindkey -M menuselect 'k' vi-up-line-or-history
+bindkey -M menuselect 'l' vi-forward-char
+bindkey -M menuselect 'j' vi-down-line-or-history
+bindkey -v '^?' backward-delete-char
 
 # Change cursor shape for different vi modes.
 function zle-keymap-select {
@@ -33,6 +35,7 @@ zle-line-init() {
     zle -K viins # initiate `vi insert` as keymap (can be removed if `bindkey -V` has been set elsewhere)
     echo -ne "\e[5 q"
 }
+
 zle -N zle-line-init
 echo -ne '\e[5 q' # Use beam shape cursor on startup.
 preexec() { echo -ne '\e[5 q' ;} # Use beam shape cursor for each new prompt.
