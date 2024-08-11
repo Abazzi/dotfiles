4d91ac853534accfed519c756ce615015e226e13
Adam Bazzi <8884041+Abazzi@users.noreply.github.com>
2024-08-11 17:21:19 -0400

    removed non-submodule nvim config

--- .config/nvim/lua/core/helpers.lua
+++
@@ -1,6 +0,0 @@
-vim.api.nvim_create_user_command('Fr', function(opts)
-  local template = ':%s/'
-  local cmd = string.format("%s/%s/gc", opts.fargs[1], opts.fargs[2])
-  local returnValue = string.format('%s%s', template, cmd)
-  vim.cmd(returnValue)
-end, { desc = "Find and Replace", nargs = '*' })
